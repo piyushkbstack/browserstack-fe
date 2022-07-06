96529ecdfbe1d64c689aa7c606412b3ff314ff9b
PIYUSH KUMAR <piyush.k@browserstack.com>
2022-07-06 21:17:54 +0530

    cucumber automate fix

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -2,15 +2,31 @@ const chromedriver = require('chromedriver');
 
 module.exports = {
   test_settings: {
-    default: {
-      webdriver: {
-        start_process: true,
-        server_path: chromedriver.path,
-        port: 4440,
-        cli_args: ['--port=4444']
+    browserstack: {
+      selenium: {
+        host: 'hub.browserstack.com',
+        port: 443
       },
+      // More info on configuring capabilities can be found on:
+      // https://www.browserstack.com/automate/capabilities?tag=selenium-4
       desiredCapabilities: {
-        browserName: 'chrome'
+        'bstack:options' : {
+          userName: '${BROWSERSTACK_USER}',
+          accessKey: '${BROWSERSTACK_KEY}',
+          buildName: 'browserstack-build-1'
+        },
+        browserName: 'chrome',
+        browserVersion: 'latest'
+      },
+
+      disable_error_log: true,
+      webdriver: {
+        timeout_options: {
+          timeout: 15000,
+          retry_attempts: 3
+        },
+        keep_alive: true,
+        start_process: false
       }
     }
   }
